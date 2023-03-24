5ef591da6f2c9d7f808d398cfc661640a589dd51
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-03-24 15:22:50 +0300

    Time: 2 ms, Memory: 40.5 MB - LeetHub

--- valid-parentheses/valid-parentheses.java
+++ valid-parentheses/valid-parentheses.java
@@ -1,21 +1,27 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stk = new Stack<>();
-        
-        char[] chars = s.toCharArray();
-        for (char c: chars) {
-            if (c == '(') stk.push(c);
-            else if (c == '{') stk.push(c);
-            else if (c == '[') stk.push(c);
-            
-            else if (stk.empty()) return false;
-            
-            else if (c == ')' && stk.pop() != '(') return false;
-            else if (c == '}' && stk.pop() != '{') return false;
-            else if (c == ']' && stk.pop() != '[') return false;
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (isOpenBracket(c)) {
+                stack.push(c);
+            } else {
+                if (stack.empty() || !isMatchingBracket(stack.pop(), c)) {
+                    return false;
+                }
+            }
         }
-        
-        if (stk.empty()) return true;
-        return false;
+
+        return stack.empty();
+    }
+
+    private boolean isOpenBracket(char c) {
+        return c == '[' || c == '{' || c == '(';
+    }
+
+    private boolean isMatchingBracket(char open, char close) {
+        return (open == '{' && close == '}') ||
+                (open == '[' && close == ']') ||
+                (open == '(' && close == ')');
     }
 }
