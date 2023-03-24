8db167cef3b08b6b75ca5cf031c7c0c56c1e35d6
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-03-24 04:39:23 +0300

    Time: 8 ms, Memory: 50.1 MB - LeetHub

--- number-of-islands/number-of-islands.java
+++ number-of-islands/number-of-islands.java
@@ -1,40 +1,43 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) return 0;
-        
-        int rows = grid.length;
-        int columns = grid[0].length;
-        int numberOfIslands = 0;
-        
+        int rowsCount = grid.length;
+        int columnsCount = grid[0].length;
+        int islandsCount = 0;
+
         int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        Queue<int[]> q = new LinkedList();
-        
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++){
-                
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < rowsCount; i++) {
+            for (int j = 0; j < columnsCount; j++) {
                 if (grid[i][j] == '1') {
-                    grid[i][j] = '2';
-                    
-                    q.add(new int[] {i, j});
-                    numberOfIslands++;
-                    
-                    while (!q.isEmpty()) {
-                        int[] coordinates = q.poll();
-                        
+                    grid[i][j] = '2';    // mark it as visited
+
+                    queue.add(new int[] {i, j});
+                    islandsCount++;
+
+                    while (!queue.isEmpty()) {
+                        int[] coordinates = queue.poll();
+
                         for (int[] dir: directions) {
-                            int r = coordinates[0] + dir[0];
-                            int c = coordinates[1] + dir[1];
-                            
-                            if (r >= 0 && r < rows && c >= 0 && c < columns && grid[r][c] == '1') {
-                                q.add(new int[] {r, c});
-                                grid[r][c] = '2';
+                            int row = coordinates[0] + dir[0];
+                            int col = coordinates[1] + dir[1];
+
+                            if (isInside(row, col, rowsCount, columnsCount)) {
+                                if (grid[row][col] == '1') {
+                                    queue.add(new int[]{row, col});
+                                    grid[row][col] = '2';
+                                }
                             }
                         }
                     }
                 }
             }
         }
-        
-        return numberOfIslands;
+
+        return islandsCount;
+    }
+
+    private boolean isInside(int row, int col, int rowsCount, int columnsCount) {
+        return row >= 0 && col >= 0 && row < rowsCount && col < columnsCount;
     }
 }
