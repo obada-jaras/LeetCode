7937558f6e425771e4a365b5c7f1271b000f3baa
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-03-26 18:39:04 +0300

    Time: 0 ms, Memory: 42.4 MB - LeetHub

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -1,20 +1,38 @@
+// "Top-down" Solution
+//class Solution {
+//    int maxDepth = 0;
+//
+//    public int maxDepth(TreeNode root) {
+//        findMaxDepth(root, 1);
+//
+//        return maxDepth;
+//    }
+//
+//    private void findMaxDepth(TreeNode node, int depth) {
+//        if (node == null) return;
+//
+//        if (node.left == null && node.right == null)
+//            maxDepth = Math.max(maxDepth, depth);
+//
+//        findMaxDepth(node.left, depth+1);
+//        findMaxDepth(node.right, depth+1);
+//    }
+//}
+
+
+
+// "Bottom-up" Solution
 class Solution {
-    int maxDepth = 0;
-
     public int maxDepth(TreeNode root) {
-        findMaxDepth(root, 1);
-
-        return maxDepth;
+        return findMaxDepth(root);
     }
 
+    private int findMaxDepth(TreeNode node) {
+        if (node == null) return 0;
 
-    private void findMaxDepth(TreeNode node, int depth) {
-        if (node == null) return;
+        int leftDepth = findMaxDepth(node.left);
+        int rightDepth = findMaxDepth(node.right);
 
-        if (node.left == null && node.right == null)
-            maxDepth = Math.max(maxDepth, depth);
-
-        findMaxDepth(node.left, depth+1);
-        findMaxDepth(node.right, depth+1);
+        return Math.max(leftDepth, rightDepth) + 1;
     }
 }
