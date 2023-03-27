f705aa0c222229004427c4760302ceb1c186ee02
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-03-28 01:11:04 +0300

    Time: 20 ms, Memory: 45.4 MB - LeetHub

---
+++ serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,54 @@
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) return "";
+        
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        
+        while(!q.isEmpty()) {
+            TreeNode node = q.poll();
+
+            if (node == null) sb.append("#,");
+            else {
+                sb.append(node.val + ",");
+
+                q.add(node.left);
+                q.add(node.right);
+            }
+        }
+        
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data == "" || data == null) return null;
+        
+        String[] nodeVals = data.split(",");
+        
+        TreeNode root = new TreeNode(Integer.parseInt(nodeVals[0]));
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        
+        for (int i = 1; i < nodeVals.length; i++) {
+            TreeNode currentNode = q.poll();
+            
+            if (!nodeVals[i].equals("#")) {
+                TreeNode leftNode = new TreeNode(Integer.parseInt(nodeVals[i]));
+                currentNode.left = leftNode;
+                q.add(leftNode);
+            }
+            
+            if (!nodeVals[++i].equals("#")) {
+                TreeNode rightNode = new TreeNode(Integer.parseInt(nodeVals[i]));
+                currentNode.right = rightNode;
+                q.add(rightNode);
+            }
+        }
+        
+        return root;
+    }
+}
