f8622eb076998efb38d085d8cad8a865dbbcb640
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-04-07 06:20:02 +0300

    Time: 1 ms, Memory: 42.3 MB - LeetHub

--- single-number/single-number.java
+++ single-number/single-number.java
@@ -2,8 +2,8 @@ class Solution {
     public int singleNumber(int[] nums) {
         int answer = 0;
         
-        for (Integer i : nums) {
-            answer ^= i;
+        for (int i = 0; i < nums.length; i++) {
+            answer ^= nums[i];
         }
         
         return answer;
