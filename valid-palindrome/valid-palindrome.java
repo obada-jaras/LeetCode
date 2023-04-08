5b45dc62d95e76bceaa3197a9aa06b98a111b636
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-04-08 06:25:53 +0300

    Time: 3 ms, Memory: 42.8 MB - LeetHub

--- valid-palindrome/valid-palindrome.java
+++ valid-palindrome/valid-palindrome.java
@@ -1,31 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = cleanText(s);
-        System.out.println(s);
-        
         int p1 = 0, p2 = s.length()-1;
         
         while (p1 <= p2) {
-            if (s.charAt(p1) != s.charAt(p2)) return false;
+            if (!Character.isLetterOrDigit(s.charAt(p1))) {
+                p1++;
+                continue;
+            }
+            
+            if (!Character.isLetterOrDigit(s.charAt(p2))) {
+                p2--;
+                continue;
+            }
+            
+            if (Character.toLowerCase(s.charAt(p1)) != Character.toLowerCase(s.charAt(p2)))
+                return false;
+            
             p1++;
             p2--;
         }
         
         return true;
-    }
-    
-    
-    private String cleanText(String str) {
-        String newStr = "";
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (c >= 'A' && c <= 'Z')
-                newStr += Character.toLowerCase(c);
-            
-            else if ((c >= 'a' && c <= 'z' ) || (c >= '0' && c <= '9')) 
-                newStr += c;
-        }
-        
-        return newStr;
     }
 }
