5ec1ecb39bd978daef2e97c01dc21739511da1ef
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-04-09 08:04:31 +0300

    Time: 1 ms, Memory: 43.2 MB - LeetHub

--- fizz-buzz/fizz-buzz.java
+++ fizz-buzz/fizz-buzz.java
@@ -6,7 +6,7 @@ class Solution {
             if (i%15 == 0) list.add("FizzBuzz");
             else if (i%3 == 0) list.add("Fizz");
             else if (i%5 == 0) list.add("Buzz");
-            else list.add(""+i);
+            else list.add(String.valueOf(i));
         }
         
         return list;
