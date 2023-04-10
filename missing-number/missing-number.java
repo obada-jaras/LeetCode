ec35f70c3b9794525c321294311cef60d09e72d2
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-04-10 06:37:45 +0300

    Time: 0 ms, Memory: 43.1 MB - LeetHub

--- missing-number/missing-number.java
+++ missing-number/missing-number.java
@@ -2,18 +2,11 @@ class Solution {
     public int missingNumber(int[] nums) {
         int size = nums.length;
         int sum = size * (size+1) / 2;
-        boolean isZero = true;
         
         for (int i = 0; i < size; i++) {
-            int num = nums[i];
-            
-            if (num == 0)
-                isZero = false;
-            
-            sum -= num;
+            sum -= nums[i];
         }
         
-        if (isZero) return 0;
         return sum;
     }
 }
