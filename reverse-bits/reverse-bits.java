ce8411819577b39354b04d6cba7dab15ab9e3304
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-04-10 06:01:17 +0300

    Time: 1 ms, Memory: 41.9 MB - LeetHub

--- reverse-bits/reverse-bits.java
+++ reverse-bits/reverse-bits.java
@@ -1,11 +1,11 @@
 public class Solution {
+    // you need treat n as an unsigned value
     public int reverseBits(int n) {
         int result = 0;
         int count = 32;
         
         while (count > 0) {
-            result <<= 1;
-            result |= (n & 1);
+            result = result * 2 + (n & 1);
             n >>>= 1;
             count--;
         }
