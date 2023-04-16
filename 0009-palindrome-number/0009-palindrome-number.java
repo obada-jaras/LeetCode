cf7caf36512d9d3ab8a8967214e3fa85fc5847d7
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-04-16 04:18:25 +0300

    Time: 11 ms (26.41%), Space: 42.1 MB (46.74%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        
+        int reversed = reverse(x);
+        if (reversed == -1) return false;
+        
+        while (x > 0) {
+            if (x%10 != reversed%10) return false;
+            x /= 10;
+            reversed /= 10;
+        }
+        
+        return true;
+    }
+    
+    private int reverse(int x) {
+        long reversed = 0;
+        
+        while (x > 0) {
+            reversed = reversed*10 + x%10;
+            x /= 10;
+        }
+        
+        if (reversed > Integer.MAX_VALUE) return -1;
+        return (int)reversed;
+    }
+}
