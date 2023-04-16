a8cb2953494a9eb33a12864a665541c4f3911d23
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-04-16 20:08:50 +0300

    Time: 0 ms (100.00%), Space: 39.4 MB (49.80%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    int[] memo;
+    
+    public int climbStairs(int n) {
+        memo = new int[n+1];
+        
+        return countWays(n);
+    }
+    
+    private int countWays(int n) {
+        if (n == 0 || n == 1) return 1;
+        
+        if (memo[n] != 0) return memo[n];
+        
+        memo[n] = countWays(n-1) + countWays(n-2);
+        return memo[n];
+    }
+}
