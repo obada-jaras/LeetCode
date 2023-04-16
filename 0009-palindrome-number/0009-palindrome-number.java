7cd1f85b991fbd7d1a7853450f1edaadd67ab1f9
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-04-16 04:26:15 +0300

    Time: 9 ms (99.24%), Space: 42.4 MB (30.20%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,26 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) return false;
+        if (x < 0 || (x != 0 && x%10 == 0)) return false;
         
-        int reversed = reverse(x);
-        
-        while (x > 0) {
-            if (x%10 != reversed%10) return false;
-            x /= 10;
-            reversed /= 10;
-        }
-        
-        return true;
-    }
-    
-    private int reverse(int x) {
-        int reversed = 0;
-        
-        while (x > 0) {
-            reversed = reversed*10 + x%10;
+        int reversedHalf = 0;
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf*10 + x%10;
             x /= 10;
         }
         
-        return reversed;
+        return (x == reversedHalf || x == reversedHalf/10);
     }
 }
