2566f74343fa667f532c8491bd357872251e39d9
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-04-19 06:15:15 +0300

    Time: 1 ms (97.00%), Space: 49.6 MB (47.58%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -3,14 +3,10 @@ class Solution {
         int low = 0, high = s.length-1;
         
         while (low < high) {
-            swap(s, low, high);
+            s[low] ^= s[high];
+            s[high] ^= s[low];
+            s[low] ^= s[high];
             low++; high--;
         }
-    }
-    
-    private void swap(char[] s, int low, int high) {
-        s[low] ^= s[high];
-        s[high] ^= s[low];
-        s[low] ^= s[high];
     }
 }
