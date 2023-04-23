0c49fe93d7e06d84d5781b139644103d8a67fa93
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-04-23 23:20:13 +0300

    Time: 4 ms (79.88%), Space: 52.2 MB (94.48%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,16 +1,26 @@
 class Solution {
     public int maxArea(int[] height) {
-        int left = 0, right = height.length-1;
-        int max = 0;
-        
-        while (left < right) {
-            int area = (right - left) * Math.min(height[left], height[right]);
-            max = Math.max(max, area);
-            
-            if (height[left] < height[right]) left++;
-            else right--;
+        int leftIndex = 0, rightIndex = height.length - 1;
+        int maxArea = 0;
+
+        // Move the left and right indices toward each other
+        while (leftIndex < rightIndex) {
+            // Calculate the area between the left and right indices
+            int minHeight = Math.min(height[leftIndex], height[rightIndex]);
+            int width = rightIndex - leftIndex;
+            int area = width * minHeight;
+
+            // Update the maximum area if the current area is larger
+            maxArea = Math.max(maxArea, area);
+
+            // Move the index with the shorter height towards the center
+            if (height[leftIndex] < height[rightIndex]) {
+                leftIndex++;
+            } else {
+                rightIndex--;
+            }
         }
-        
-        return max;
+
+        return maxArea;
     }
 }
