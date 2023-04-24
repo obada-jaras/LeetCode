0bd3a31e6e3e15980b3927ead953c7f600f854f1
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-04-24 05:23:43 +0300

    Time: 7 ms (78.95%), Space: 42.6 MB (47.20%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,25 +1,29 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int[] count = new int[26];
-        int left = 0, right = 0;
-        int maxCount = 0, maxLength = 0;
-        
-        while (right < s.length()) {
-            count[s.charAt(right) - 'A']++;
-            maxCount = Math.max(maxCount, count[s.charAt(right) - 'A']);
-            
-            int windowSize = right - left + 1;
-            int neededReplacements = windowSize - maxCount;
-            if (neededReplacements > k) {
-                count[s.charAt(left) - 'A']--;
-                left++;
+        int[] charFrequency = new int[26];
+        int leftPointer = 0, rightPointer = 0;
+        int maxCharFrequency = 0, longestSubstring = 0;
+
+        // Iterate through the string using a sliding window approach
+        while (rightPointer < s.length()) {
+            char currentChar = s.charAt(rightPointer);
+            charFrequency[currentChar - 'A']++;
+            maxCharFrequency = Math.max(maxCharFrequency, charFrequency[currentChar - 'A']);
+
+            int windowSize = rightPointer - leftPointer + 1;
+            int replacementsNeeded = windowSize - maxCharFrequency;
+
+            // If we need more replacements than allowed, shrink the window
+            if (replacementsNeeded > k) {
+                charFrequency[s.charAt(leftPointer) - 'A']--;
+                leftPointer++;
+            } else {
+                // Update longest substring only if the current window is valid
+                longestSubstring = Math.max(longestSubstring, windowSize);
             }
-            else {
-                maxLength = Math.max(maxLength, windowSize);
-            }
-            right++;
+            rightPointer++;
         }
-        
-        return maxLength;
+
+        return longestSubstring;
     }
 }
