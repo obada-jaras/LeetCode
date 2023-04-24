0a5793ef75b32d0b9e9e8f5d7d2c391ee5407226
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-04-24 05:05:38 +0300

    Time: 8 ms (69.93%), Space: 42.6 MB (35.12%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,29 +1,23 @@
-class Solution {
+public class Solution {
     public int characterReplacement(String s, int k) {
-        HashMap<Character, Integer> hash = new HashMap<>();
-        int leftIndex = 0, rightIndex = 0;
+        int[] count = new int[26];
+        int left = 0;
         int maxCount = 0;
         int maxLength = 0;
-        
-        while (rightIndex < s.length()) {
-            char ch = s.charAt(rightIndex);
-            hash.put(ch, hash.getOrDefault(ch, 0) + 1);
-            maxCount = Math.max(maxCount, hash.get(ch));
-            
-            int windowSize = rightIndex - leftIndex + 1;
-            int neededReplacements = windowSize - maxCount;
-            
-            if (neededReplacements > k) {
-                char leftChar = s.charAt(leftIndex);
-                hash.put(leftChar, hash.get(leftChar) - 1);
-                leftIndex++;
-            } else {
-                maxLength = Math.max(maxLength, windowSize);
+
+        for (int right = 0; right < s.length(); right++) {
+            char currentChar = s.charAt(right);
+            count[currentChar - 'A']++;
+            maxCount = Math.max(maxCount, count[currentChar - 'A']);
+
+            if (right - left + 1 - maxCount > k) {
+                count[s.charAt(left) - 'A']--;
+                left++;
             }
-            
-            rightIndex++;
+
+            maxLength = Math.max(maxLength, right - left + 1);
         }
-        
+
         return maxLength;
     }
 }
