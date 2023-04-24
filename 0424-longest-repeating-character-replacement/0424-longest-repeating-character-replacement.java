baa299d541486bc36927c2c2de5bf1a4061c5c42
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-04-24 05:07:27 +0300

    Time: 10 ms (54.56%), Space: 42.3 MB (67.65%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,23 +1,16 @@
-public class Solution {
+class Solution {
     public int characterReplacement(String s, int k) {
-        int[] count = new int[26];
-        int left = 0;
-        int maxCount = 0;
-        int maxLength = 0;
-
-        for (int right = 0; right < s.length(); right++) {
-            char currentChar = s.charAt(right);
-            count[currentChar - 'A']++;
-            maxCount = Math.max(maxCount, count[currentChar - 'A']);
-
-            if (right - left + 1 - maxCount > k) {
-                count[s.charAt(left) - 'A']--;
-                left++;
+        int[] arr = new int[26];
+        int largestCount = 0, beg = 0, maxlen = 0;
+        for(int end = 0; end < s.length(); end ++){
+            arr[s.charAt(end) - 'A']++;
+            largestCount = Math.max(largestCount, arr[s.charAt(end) - 'A']);
+            if(end - beg + 1 - largestCount > k){     
+                arr[s.charAt(beg) - 'A']--;
+                beg ++;
             }
-
-            maxLength = Math.max(maxLength, right - left + 1);
+            maxlen = Math.max(maxlen, end - beg + 1);     
         }
-
-        return maxLength;
+        return maxlen;
     }
 }
