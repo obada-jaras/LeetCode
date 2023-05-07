5483822d3bbcf558411756ea79d00810ab84874b
OBADA TAHAYNA <35347949+obada-jaras@users.noreply.github.com>
2023-05-07 05:24:55 +0300

    Time: 0 ms (100.00%), Space: 42.3 MB (40.20%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,9 +1,30 @@
+// O(n)
+// class Solution {
+//     public int findMin(int[] nums) {
+//         for (int i = 0; i < nums.length-1; i++) {
+//             if (nums[i+1] < nums[i]) return nums[i+1];
+//         }
+        
+//         return nums[0];
+//     }
+// }
+
+
+// O(log n)
 class Solution {
     public int findMin(int[] nums) {
-        for (int i = 0; i < nums.length-1; i++) {
-            if (nums[i+1] < nums[i]) return nums[i+1];
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
         }
-        
-        return nums[0];
+
+        return nums[left];
     }
 }
